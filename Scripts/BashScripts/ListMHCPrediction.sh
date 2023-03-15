5932a9e3455ac884f981b9553b577649ae64c159
Sechyss <albertotr92@gmail.com>
2023-03-15 12:35:32 +0000

    Commit

--- Scripts/BashScripts/ListMHCPrediction.sh
+++ Scripts/BashScripts/ListMHCPrediction.sh
@@ -1,1 +1,1 @@
-python src/predict_binding.py consensus mhc | cut -f2 | grep -e 'HLA'| uniq > /Users/u2176312/OneDrive\ -\ University\ of\ Warwick/CSP/ListMHC_humans_consensus.txt
+python src/predict_binding.py consensus mhc | grep -e 'HLA'| uniq > /Users/u2176312/OneDrive\ -\ University\ of\ Warwick/CSP/ListMHC_humans_consensus.txt
