d5a2f634a559b0f7ee6f2d05af6e99895e994782
Sechyss <albertotr92@gmail.com>
2023-03-28 14:12:16 +0100

    Commit

--- Scripts/Pythonconsole/BarchartHLAsKmers.py
+++ Scripts/Pythonconsole/BarchartHLAsKmers.py
@@ -25,19 +25,20 @@ number_variants = Kenya_region['Variants'].to_numpy()
 
 def extractionofpercentages(dictionary):
     list_percentages = []
-    counter = 0
     values = list(dictionary.values())
     total_values = list(set(item for sublist in values for item in sublist))
+    total_values.sort()
+    counter = 0
     for ids in number_variants:
-        list_ids = range(counter + ids)
-        totalids = len(list_ids)
+        list_ids = range(counter, ids + counter)
         hits = 0
         for seqid in list_ids:
             if seqid in total_values:
                 hits += 1
-        percentage_of_hits = round((hits/totalids * 100), 2)
+        percentage_of_hits = round((hits/ids * 100), 2)
         list_percentages.append(percentage_of_hits)
         counter += ids
+
     return list_percentages
 
 
@@ -57,10 +58,10 @@ add_labels(x_axis, number_variants, axis=ax1)
 ax1.set_title('Number of variants', loc='left', y=1, weight='bold')
 ax2.bar(x=x_axis, height=hitsKenya, edgecolor='black', color='green')
 add_labels(x_axis, hitsKenya, axis=ax2)
-ax2.set_title('Kenya Population', loc='left', y=1, weight='bold')
+ax2.set_title('Kenya Population (% of variants recognised)', loc='left', y=1, weight='bold')
 ax3.bar(x=x_axis, height=hitsFinland, edgecolor='black', color='red')
 add_labels(x_axis, hitsFinland, axis=ax3)
-ax3.set_title('Finland Population', loc='left', y=1, weight='bold')
+ax3.set_title('Finland Population (% of variants recognised)', loc='left', y=1, weight='bold')
 
 plt.xticks(fontsize=9, fontweight='bold')
 plt.savefig('/Users/u2176312/OneDrive - University of Warwick/CSP/CSP_SNP_region/'
