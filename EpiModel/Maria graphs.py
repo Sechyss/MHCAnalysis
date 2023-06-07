ddd901910412cb426aaaf8d4ece7581b0ca3fb20
Sechyss <albertotr92@gmail.com>
2023-06-07 10:09:09 +0100

    Commit

--- EpiModel/Maria graphs.py
+++ EpiModel/Maria graphs.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# %%
+
 phi_values = pd.read_excel('/Users/u2176312/OneDrive - University of Warwick/Miniproject/Aphids/Copy of phi values['
                            '17].xlsx', sheet_name='Sheet4', index_col=0)
 phi_values = phi_values.transpose()
@@ -19,8 +21,7 @@ plt.show()
 # %%
 
 aphids = pd.read_excel('/Users/u2176312/OneDrive - University of Warwick/Miniproject/Aphids/Copy of phi values['
-                           '17].xlsx', sheet_name='Sheet2', index_col=0)
-
+                       '17].xlsx', sheet_name='Sheet2', index_col=0)
 
 fig, ax = plt.subplots(1, 1)
 
@@ -30,3 +31,27 @@ plt.xlabel('Number of viruliferous aphids', fontweight='bold', fontsize=12)
 plt.ylabel('Percentage of cabbage infection (%)', fontweight='bold', fontsize=12)
 plt.savefig('Optimization of viruliferous aphids.pdf', dpi=300)
 plt.show()
+
+# %% Plot log equation
+
+from scipy import optimize
+
+
+x = [100, 19015, 983, 664, 2, 58, 9179, 26588, 295, 31587, 503, 16389]
+y = [91.9, 97.2, 66.6, 95.6, 8.1, 19.8, 40.2, 98.8, 26.5, 91.5, 98.0, 99.7]
+x.sort()
+y.sort()
+
+def log_equation(x_value, intercept, slope):
+    import numpy as np
+    return intercept + slope * np.log(x_value)
+
+
+p0 = [0.0001, 0.0001]
+params, cv = optimize.curve_fit(log_equation, xdata=x, ydata=y, p0=p0)
+plt.plot(x, y, 'o')
+plt.plot(x, log_equation(x, params[0], params[1]), 'red')
+plt.text(5000, 10, u'y = '+str(round(params[0],2))+'+'+str(round(params[1], 2))+'log(x)', style='italic')
+
+plt.show()
+
