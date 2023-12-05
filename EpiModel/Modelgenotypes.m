8ff818eb8e4baf1dec648077bbe12cdfc0af49ce
Alberto Torcello <82230842+Sechyss@users.noreply.github.com>
2023-12-05 09:48:40 +0000

    Update Modelgenotypes.m

--- EpiModel/Modelgenotypes.m
+++ EpiModel/Modelgenotypes.m
@@ -8,8 +8,8 @@ Ratio33 = 0.1;
 ratios = [Ratio12 Ratio11 Ratio22 Ratio33];
 
 betavalues = [0.002 0.002 0.002 0.002];
-time_new = 200;
-Time_simulation = 800;
+time_new = 200;  % Time of introduction of the new strain 
+Time_simulation = 800;  % Total time for the simulation (in days)
 new_Strain = '1b2a';
 seeds = 42;
 parameters = {betavalues, time_new, Time_simulation, new_Strain seeds};
