0c0452804be376bfe0077da287ed3d9b98f22063
Sechyss <albertotr92@gmail.com>
2024-03-11 15:55:25 +0000

    Commit

--- Scripts/Pythonconsole/Alignment_Coordinates_test.py
+++ Scripts/Pythonconsole/Alignment_Coordinates_test.py
@@ -1,40 +1,19 @@
+import os
+
 import pandas as pd
 import pickle
-import argparse
-import textwrap
 from Bio import AlignIO, SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from tqdm import tqdm
 
+os.chdir('/Users/u2176312/OneDrive - University of Warwick/Otherproteins/Multiple_Alignment/')
 
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=textwrap.dedent('''\
-This script takes a multiple protein alignment file and a list of column names,
-and returns a fasta file with kmer sequences with alignment coordinates based on a given reference ID.
-
-Additionally it returns a dictionary with a key for each kmer and the IDs associated with that kmer.
-
-'''))
-
-parser.add_argument("-a", "--alignment", metavar='MUSCLE', dest="sequences", help="Alignment in fasta",
-                    type=str, required=True)
-parser.add_argument('-s', '--starting', metavar='starting_aa', dest="starting_point",
-                    help="Starting aa position", type=int)
-parser.add_argument("-r", "--reference", dest="ref_id", help="Reference ID", type=str,  required=True)
-parser.add_argument("-o", "--output", metavar='file.fasta', dest="output", help="Output file to save",
-                    type=str,  required=True)
-
-
-args = parser.parse_args()
-
-# #######################################################################################################################
-
-alignment = AlignIO.read(args.sequences, format="fasta")
-counter = args.starting_point
+alignment = AlignIO.read('Liverstageantigen1_Nterminal.muscle.faa', format="fasta")
+counter = 1
 coordinates = []
 for record in alignment:
-    if record.id == args.ref_id:
+    if record.id == 'XP_024329106.1':
         sequence = record.seq
         for aminoacid in sequence:
             if aminoacid != '-':
@@ -47,7 +26,7 @@ for record in alignment:
 
 df = pd.DataFrame(index=[records.id for records in alignment], columns=coordinates)
 
-print('Collecting coordinates for ' + args.output)
+print('Collecting coordinates for ' + 'Liverstageantigen1_Nterminal')
 for i, col in tqdm(enumerate(alignment)):
     aligment_id = col.id
     all_seq = col.seq
@@ -55,9 +34,9 @@ for i, col in tqdm(enumerate(alignment)):
         df.loc[aligment_id, coordinates[index]] = aminoacid
 
 Gene_depth = {}
-print('Estimating the gene depth for ' + args.output)
+print('Estimating the gene depth for ' + 'Liverstageantigen1_Nterminal')
 ending_pos = 0
-for position in tqdm(range(len(df.columns))):
+for position in range(len(df.columns)):
     if ending_pos != df.columns[-1]:
         starting_pos = df.columns[position]
         ending_pos = df.columns[position + 10]
@@ -68,8 +47,8 @@ for position in tqdm(range(len(df.columns))):
 
 Sequences_dictionary = {}
 
-print('Writing Kmer sequences for ' + args.output)
-with open(str(args.output) + '_kmers.fasta', 'a') as f:
+print('Writing Kmer sequences for ' + 'Liverstageantigen1_Nterminal')
+with open(str('Liverstageantigen1_Nterminal') + '_kmers.fasta', 'a') as f:
 
     for index, row in tqdm(df.iterrows()):
         filtered_row = row[row != '-']
@@ -83,7 +62,7 @@ with open(str(args.output) + '_kmers.fasta', 'a') as f:
                 ending_pos = all_coordinates[position + 10]
 
                 kmer = "".join(filtered_row.values[position: position + 11])
-                id_kmer = "".join([args.output, '_', str(starting_pos), '_', str(ending_pos), '_', index])
+                id_kmer = "".join(['Liverstageantigen1_Nterminal', '_', str(starting_pos), '_', str(ending_pos), '_', index])
                 if kmer not in Sequences_dictionary.keys():
                     Sequences_dictionary.update({kmer: [id_kmer]})
                 else:
@@ -94,20 +73,17 @@ with open(str(args.output) + '_kmers.fasta', 'a') as f:
                 continue
 
 
-with open(str(args.output) + '.pickle', 'wb') as f2:
+with open(str('Liverstageantigen1_Nterminal') + '.pickle', 'wb') as f2:
     pickle.dump(Sequences_dictionary, f2)
 
-with open(str(args.output) + '_Kmer_depth.pickle', 'wb') as f3:
-    pickle.dump(Gene_depth, f3)
-
-with open(str(args.output) + '_kmers_filtered.fasta', 'a') as f4:
+with open(str('Liverstageantigen1_Nterminal') + '_kmers_filtered.fasta', 'a') as f3:
     Sequence_counter = 0
 
     for key in tqdm(Sequences_dictionary.keys()):
         peptide = key
         sequence_id = 'Sequence_' + str(Sequence_counter)
         seq_record = SeqRecord(Seq(peptide), id=sequence_id, description='')
-        SeqIO.write(seq_record, f4, 'fasta')
+        SeqIO.write(seq_record, f3, 'fasta')
         Sequence_counter += 1
 
 
